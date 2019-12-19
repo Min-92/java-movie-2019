cea63469ac24c5413371f08de7c8e15fcaff3444
Min-92 <0201.mj.lee@gmail.com>
2019-12-19 16:30:38 +0900

    refactor : idMap 필드 변경
    - 생성자를 이용해서 idMap 을 필드로 변경

--- src/main/java/app/Booking.java
+++ src/main/java/app/Booking.java
@@ -10,25 +10,23 @@ import view.OutputView;
 public class Booking {
 
   private List<Movie> movies;
+  private HashMap<Integer, Boolean> idMap = new HashMap<>();
 
   public Booking() {
     movies = MovieRepository.getMovies();
-  }
-
-  public void start() {
-    showMovieList();
-
-    int movieId = getBookingMovieId();
-
-  }
-
-
-  private void validateMovieId(int inputId) throws Exception {
-    HashMap<Integer, Boolean> idMap = new HashMap<>();
 
     for (Movie movie : movies) {
       idMap.put(movie.getId(), true);
     }
+  }
+
+  public void start() {
+    showMovieList();
+    int movieId = getBookingMovieId();
+
+  }
+
+  private void validateMovieId(int inputId) throws Exception {
 
     if (!idMap.containsKey(inputId)) {
       throw new Exception("영화 ID가 존재하지 않습니다.");
