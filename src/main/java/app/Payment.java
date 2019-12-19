84792f84d9484dbc28111971ccf3735a69b47449
Min-92 <0201.mj.lee@gmail.com>
2019-12-19 18:22:30 +0900

    fix : getPoint() return 값 수정
    - int를 리턴하도록 수정

--- src/main/java/app/Payment.java
+++ src/main/java/app/Payment.java
@@ -17,16 +17,17 @@ public class Payment {
 
   }
 
-  private void validatePoint(int point) throws Exception {
-    if(point < 0){
+  private int validatePoint(int point) throws Exception {
+    if (point < 0) {
       throw new Exception("0보다 크거나 같아야 합니다.");
     }
+    return point;
   }
 
   private int getPoint() {
     try {
       int point = InputView.inputPoint();
-      validatePoint(point);
+      return validatePoint(point);
     } catch (Exception e) {
       System.out.println(e.getMessage());
       return getPoint();
